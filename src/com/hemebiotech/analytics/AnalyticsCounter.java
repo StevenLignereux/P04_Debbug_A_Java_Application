402e090a0f380d05c73aa2033a088d190dd9ee69
StevenLignereux <slignere@gmail.com>
2022-08-30 17:21:08 +0200

    <refactor>(<main>): <reformatting main function for use classes>
    <description> Creating a class for count symptoms occurences. Refactor main function for using ReadSymptomDataFromfile and CountSymptom

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,41 +1,24 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class AnalyticsCounter {
 
     public static void main(String[] args) throws Exception {
-        // Use the maps to extracting the symptoms of the file
-        File symptomList = new File("symptoms.txt");
-        Map<String, Integer> mapSymptomWithNumberOccurence = new TreeMap<>();
-        FileReader symptomListReader = new FileReader(symptomList);
-        BufferedReader bufferedSymptomReader = new BufferedReader(symptomListReader);
+        ReadSymptomDataFromFile symptomReader = new ReadSymptomDataFromFile("symptoms.txt");
+        List<String>listOfSymptoms = symptomReader.GetSymptoms();
 
-        // one symptom in the list
-        String symptomStr;
-
-        while ((symptomStr = bufferedSymptomReader.readLine()) != null) {
-            if (mapSymptomWithNumberOccurence.containsKey(symptomStr)) {
-                int countOccurence = mapSymptomWithNumberOccurence.get(symptomStr);
-                countOccurence++;
-                mapSymptomWithNumberOccurence.replace(symptomStr, countOccurence);
-            } else {
-                mapSymptomWithNumberOccurence.put(symptomStr, 1);
-            }
-        }
+        CountSymptom countSymptomInstance = new CountSymptom(listOfSymptoms);
+        Map<String, Integer> resultSymptom = countSymptomInstance.getMapOfSymptomsWithNumberOccurences();
 
         // next generate output
         FileWriter writerSymptom = new FileWriter("result.out");
-        for (String symptom : mapSymptomWithNumberOccurence.keySet()) {
-            System.out.println(symptom + "=" + mapSymptomWithNumberOccurence.get(symptom));
-            writerSymptom.write(symptom + "=" + mapSymptomWithNumberOccurence.get(symptom) + "\n");
+        for (String symptom : resultSymptom.keySet()) {
+            System.out.println(symptom + "=" + resultSymptom.get(symptom));
+            writerSymptom.write(symptom + "=" + resultSymptom.get(symptom) + "\n");
         }
-        symptomListReader.close();
         writerSymptom.close();
     }
 }
